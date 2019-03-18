0364f99115b50921d9f8e6797b66f85898ac13ad
Michael Labib <javaone123@yahoo.com>
2019-03-18 20:32:58 +0000

    Update lazy_load.py
    added sample object

--- examples/lazyload/lazy_load.py
+++ examples/lazyload/lazy_load.py
@@ -4,32 +4,30 @@
 import boto3
 import datetime
 import redis
+import sys
+sys.path.insert(0,'amazon-S3-cache-with-amazon-elasticache-redis/resources/')
+import constants
 
-### Required: update Redis endpoint information ###
-redishost=""
-### Required: update S3 bucket information ###
-S3bucket = ""
-
-r = redis.StrictRedis(host=redishost, port=6379)
+r = redis.StrictRedis(host=constants.redishost, port=6379)
 
 s3 = boto3.resource('s3')
-### Check to see if value is in Redis
 
-value = r.get(S3bucket + ':' + S3ObjectKey)
+###Sample object to test with
+S3ObjectKey = "filename0.txt"
+
+### Check to see if value is in Redis
+value = r.get(constants.S3bucket + ':' + S3ObjectKey)
 
 if value is None:
     print("Cache Miss")
     ### Get data from S3
  
-    obj = s3.Object(S3bucket, S3ObjectKey)
+    obj = s3.Object(constants.S3bucket, S3ObjectKey)
     data = obj.get()['Body'].read().decode('utf-8') 
     
     ### Store the data into Redis
-    r.set(S3bucket + ':' + S3ObjectKey, data)
+    r.set(constants.S3bucket + ':' + S3ObjectKey, data)
 
 else:
     print ("Cache Hit")
     print("Data retrieved from redis = " + value)
-
-
-
